de9685e60fc0fc3aaf021265c1e987f80986535d
reybaliguat <rey@ajio.co.jp>
2021-01-19 11:57:54 +0800

    few changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Weather from './components/pages/Weather/Weather';
 
 
 function App() {
     return (
-      <BrowserRouter>
-        <Switch>
-            <Route exact path={'/'} component={Weather} />
-        </Switch>
-      </BrowserRouter>
+      <div>
+        <Weather />
+      </div>
     );
 }
 
